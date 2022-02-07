21beda14a61676b1b1753ea5c9146e8d5a331e84
Egor Plutakhin <nikapriya@gmail.com>
2022-02-07 21:55:43 +0300

    C помощью onClick добавили возможность перехода со второго экрана на главный при нажатии кнопки btnActOne. С помощью класса Build получили информацию о телефоне(Модель, Производитель и версия ОС) и передали ее в строчный массив PhoneInfo. Добавили ArrayAdapter, с помощью которого передали их в ListView и вывели на каждой отдельной строке.

--- app/src/main/java/com/example/testapp/PhoneInfo.java
+++ app/src/main/java/com/example/testapp/PhoneInfo.java
@@ -2,13 +2,52 @@ package com.example.testapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class PhoneInfo extends AppCompatActivity {
 
+    Button btnActOne;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phone_info);
+        setContentView(R.layout.activity_phone_info);btnActOne = (Button) findViewById(R.id.btnActOne);
+        btnActOne.setOnClickListener(this::onClick);
+        ListView listView = findViewById(R.id. list ) ;
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, PhoneInfo);
+        listView.setAdapter(adapter);
+    }
+
+    final String[] PhoneInfo = new String[]
+            {
+                    getPhoneModel (), getPhoneManufacturer (), getPhoneOSRelease ()
+            };
+    private String getPhoneModel () {
+        return "Phone Model: " + Build.MODEL;
+    }
+    private String getPhoneManufacturer () {
+        return "Manufacturer: " + Build.MANUFACTURER;
+    }
+    private String getPhoneOSRelease ()  {
+        return "AndroidVersion: " + Build.VERSION.RELEASE;
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnActOne:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
