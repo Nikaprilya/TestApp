72e83b1b1a5885b6ff63d464a01a0a2c787f97ff
Egor Plutakhin <nikapriya@gmail.com>
2022-02-07 20:43:46 +0300

    Добавим в файл .cpp заголовочный файл jni.h для работы с JNI и string класс для работы со строками. В файле будут находиться две функции jobjectArray : phone и name. Они будут возвращать по одному строчному массиву.

--- app/src/main/cpp/phonebook.cpp
+++ app/src/main/cpp/phonebook.cpp
@@ -1,17 +1,44 @@
-// Write C++ code here.
-//
-// Do not forget to dynamically load the C++ library into your application.
-//
-// For instance,
-//
-// In MainActivity.java:
-//    static {
-//       System.loadLibrary("testapp");
-//    }
-//
-// Or, in MainActivity.kt:
-//    companion object {
-//      init {
-//         System.loadLibrary("testapp")
-//      }
-//    }
+#include <jni.h>
+#include <string>
+
+using namespace std;
+extern "C" JNIEXPORT jobjectArray JNICALL Java_com_example_a2_MainActivity_phone(JNIEnv *env, jobject jobj){
+
+    jobjectArray ter;
+    int i;
+    const char *phone[5]= {"89003009030",
+                           "89003009031",
+                           "89003009032",
+                           "89003009033",
+                           "89003009034"};
+    ter= (jobjectArray)env->NewObjectArray(5,
+                                           env->FindClass("java/lang/String"),
+                                           env->NewStringUTF(""));
+
+    for (i=0;i<5;i++) {
+        env->SetObjectArrayElement(
+                ter,i,env->NewStringUTF(phone[i]));
+    }
+    return(ter);
+}
+
+extern "C" JNIEXPORT jobjectArray JNICALL Java_com_example_a2_MainActivity_name (JNIEnv *env, jobject jobj){
+
+    jobjectArray ret;
+    int i;
+
+    const char *name[5]= {"Petrov",
+                          "Egorov",
+                          "Ivanov",
+                          "Sidarov",
+                          "Istomin"};
+    ret = (jobjectArray)env->NewObjectArray(5,
+                                            env->FindClass("java/lang/String"),
+                                            env->NewStringUTF(""));
+
+    for(i=0;i<5;i++)  {
+        env->SetObjectArrayElement(
+                ret, i, env->NewStringUTF(name[i]));
+    }
+    return(ret);
+}
